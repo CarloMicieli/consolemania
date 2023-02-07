8a66233430aac0ed999fc823a8c6e9a18ddc9827
CarloMicieli <carlo.micieli@gmail.com>
2023-02-07 20:20:07 +0100

    test(games service): add the integration tests

--- games-service/build.gradle.kts
+++ games-service/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
@@ -21,10 +25,6 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("io.rest-assured:rest-assured")
 }
 
 tasks.getByName<BootRun>("bootRun") {
@@ -37,4 +37,50 @@ dependencyManagement {
     imports {
         mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
     }
+}
+
+@Suppress("UnstableApiUsage")
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+
+        val integrationTest by registering(JvmTestSuite::class) {
+            dependencies {
+                implementation(project())
+                implementation("org.springframework.boot:spring-boot-starter-test")
+
+                implementation("org.testcontainers:testcontainers")
+                implementation("org.testcontainers:junit-jupiter")
+                implementation("org.testcontainers:postgresql")
+                implementation("io.rest-assured:rest-assured")
+            }
+
+            targets {
+                all {
+                    testTask.configure {
+                        shouldRunAfter(test)
+                        minHeapSize = "1G"
+                        maxHeapSize = "2G"
+
+                        failFast = false
+
+                        testLogging {
+                            showStandardStreams = false
+                            events(PASSED, FAILED, SKIPPED)
+                            showExceptions = true
+                            showCauses = true
+                            showStackTraces = true
+                            exceptionFormat = FULL
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.named("check") {
+    dependsOn(testing.suites.named("integrationTest"))
 }
