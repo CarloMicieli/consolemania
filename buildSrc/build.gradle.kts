c2f157880cdebd09c7bf6261f8ac14adf15ab129
CarloMicieli <carlo.micieli@gmail.com>
2023-03-20 21:03:21 +0100

    build: update the gradle plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,10 +9,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.15.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.17.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
     implementation("me.qoomon:gradle-git-versioning-plugin:6.4.2")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.4")
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.45.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
     implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.20")
 }
